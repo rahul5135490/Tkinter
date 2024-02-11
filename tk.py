498ff1c144c6c30dd043d82318e4c0f5d36dcb1d
Rahul Srivastava <97574477+rahul5135490@users.noreply.github.com>
2024-02-11 23:39:41 +0530

    Add files via upload

---
+++ tk.py
@@ -0,0 +1,7 @@
+from tkinter import *
+rahul_root=Tk()
+# width X height
+rahul_root.geometry("664x444")
+sudha=Label(text="Sudha is a good mom, and this is her gui")
+sudha.pack()
+rahul_root.mainloop()
